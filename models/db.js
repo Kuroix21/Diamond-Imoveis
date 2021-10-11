ac4253a061e0f61f13cbd2835c17d6634c7e3d0c
@Kuro21 <leandrocesardasilvarodrigues@gmail.com>
2021-10-11 15:05:32 -0300

    ajustesFront

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('diamond_db', 'root', 'root', {dialect: 'mysql', host: 'localhost', port: 3006});
+const sequelize = new Sequelize('diamond_db', 'root', '12345', {dialect: 'mysql', host: 'localhost', port: 3306});
  
 module.exports = {
     Sequelize: Sequelize,
